f900e8f2c5089e3baf80fae2f55e4499c914844a
Kiyanox <xmytrox@gmail.com>
2022-01-09 00:37:04 +0100

    Projektdateien hinzufügen.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace file_management_tool_gt
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}

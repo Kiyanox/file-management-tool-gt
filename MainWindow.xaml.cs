e7a4d947b6a534d5638ee392571aafb67a61c280
Kiyanox <xmytrox@gmail.com>
2022-01-09 21:14:09 +0100

    Added File selection and root folder selection

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,21 +8,82 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.IO;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace file_management_tool_gt
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+
+        private String folderPath = "";
+        private bool folderSelected = false;
+
+        // Setter functions
+
+       
+        public void SetFolderPath(String path) { 
+           this.folderPath = path;
+        }
+
+        public void SetFolderSelected(bool isFolderSelected)
+        {
+            this.folderSelected = isFolderSelected;
+        }
+
+        // Getter functions
+        public String getFolderPath()
+        {
+            return this.folderPath;
+        }
+        public bool getIfFolderSelected()
+        {
+            return this.folderSelected;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void selectFiles(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Jar Files: (.jar)|*.jar";
+            if(openFileDialog.ShowDialog() == true)
+            {
+                foreach(string fileName in openFileDialog.FileNames)
+                {
+                    selectedFiles.Items.Add(Path.GetFileName(fileName));
+                }
+            }
+        }
+        private void selectRootFolderPath(object sender, RoutedEventArgs e)
+        {
+            String actualFolderPath = selectFolder();
+
+            if(actualFolderPath != "")
+            {
+                SetFolderSelected(true);
+            }
+        }
+
+        private static String selectFolder()
+        {
+            var ofd = new System.Windows.Forms.FolderBrowserDialog();
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                return ofd.SelectedPath;
+            } 
+            else
+            {
+                return "";
+            }
         }
     }
 }
